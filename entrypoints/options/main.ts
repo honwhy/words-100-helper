ce52e2c2a25ea0b4f599cb1d52061b10e5d3dc52
honwhy.wang <honwhy.wang@hotmail.com>
2024-04-07 02:45:58 +0800

    remove directive

--- entrypoints/options/main.ts
+++ entrypoints/options/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import './bootstrap.css'
 import './option.css'
 import App from './index.vue'
-import iframeResize from './iframeResize'
 
 const app = createApp(App)
-app.directive('resize', iframeResize)
 app.mount('#app')
